98e9f93eb180898b22c52bc4e77a031ed7003a31
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-04-04 19:40:12 -0400

    feat: baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { useState } from 'react';
-import { useNuiEvent } from './hooks/useNuiEvent';
-import { Box, Paper, Text } from '@mantine/core';
+import { Box, Text } from '@mantine/core';
 import classes from './index.module.css';
 
 export default function App() {
-  const [visible, setVisible] = useState(false);
-
-  useNuiEvent('setVisible', (data: { visible: boolean; }) => {
-    setVisible(data.visible);
-  });
-
   return (
-    <>
-      {visible && (
-        <Box className={classes.container}>
-          <Paper className={classes.main} shadow='sm' p='sm'>
-            <Text>Hello</Text>
-          </Paper>
-        </Box>
-      )}
-    </>
+    <Box className={classes.container}>
+      <Text>Hello :)</Text>
+    </Box>
   );
 };
