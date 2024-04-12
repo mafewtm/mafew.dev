b251db478eedebf9eaf9128ce2d11726a4fb7c3b
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-04-12 12:39:06 -0400

    feat: social media links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import { Box, Stack, Title, Text } from '@mantine/core';
+import { Box, Stack, Title, Text, Group, UnstyledButton } from '@mantine/core';
 import ColorToggle from './components/colortoggle/colortoggle';
 import classes from './index.module.css';
+import { FaGithub } from 'react-icons/fa';
+import { SiKofi } from 'react-icons/si';
+
+interface SocialMediaProps {
+  icon: any;
+  link: string;
+}
+
+function SocialMedia({ icon: Icon, link }: SocialMediaProps) {
+	return (
+    <UnstyledButton component='a' href={link}>
+      <Icon />
+    </UnstyledButton>
+	);
+}
 
 export default function App() {
   return (
@@ -10,6 +25,10 @@ export default function App() {
         <Text c='dimmed' size='lg' ta='center' className={classes.description}>
           Nothing to see here yet. More to come soon!
         </Text>
+        <Group className={classes.socialMedia}>
+          <SocialMedia icon={FaGithub} link='https://github.com/mafewtm' />
+          <SocialMedia icon={SiKofi} link='https://ko-fi.com/mafewtm' />
+        </Group>
       </Stack>
       <ColorToggle />
     </Box>
