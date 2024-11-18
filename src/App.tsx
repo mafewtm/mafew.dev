e002d9290f253d014d2204075c1158b10397ab6a
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-11-18 00:51:19 -0500

    fix(web/app): adjust discord position

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ export default function App() {
       <h1 className='font-bold text-4xl text-[#c9c9c9]'>Hi, I'm Matthew :)</h1>
       <p className='m-0 p-0 text-lg text-[#828282]'>Nothing to see here yet. More to come soon!</p>
       <div className='flex flex-row justify-center items-center gap-4 text-[#c9c9c9]'>
-        <a href='https://discord.com/users/272829359117697025' target='_blank' rel='noopener noreferrer'>
-          <Icon icon='simple-icons:discord' aria-label='Discord' />
-        </a>
         <a href='https://github.com/mafewtm' target='_blank' rel='noopener noreferrer'>
           <Icon icon='simple-icons:github' aria-label='GitHub' />
+        </a>
+        <a href='https://discord.com/users/272829359117697025' target='_blank' rel='noopener noreferrer'>
+          <Icon icon='simple-icons:discord' aria-label='Discord' />
         </a>
         <a href='https://ko-fi.com/mafewtm' target='_blank' rel='noopener noreferrer'>
           <Icon icon='simple-icons:kofi' aria-label='Ko-fi' />
