306bd399154e567528ebd35d7f484aa49f2156ad
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-01-23 20:49:28 -0500

    chore: update all packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
   ],
   base: './',
