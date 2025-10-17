7bf1071f232631fa6b38055530825726f01ee41b
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-10-17 01:40:21 -0400

    reefactor: replace iconify icons with react-simple-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Icon } from '@iconify/react';
+import { SiDiscord, SiGithub, SiMinutemailer, SiKofi } from '@icons-pack/react-simple-icons';
 
 export default function App() {
   return (
@@ -7,16 +7,16 @@ export default function App() {
       <p className='m-0 p-0 text-lg text-[#828282]'>Nothing to see here yet. More to come soon!</p>
       <div className='flex flex-row justify-center items-center gap-4 text-[#c9c9c9]'>
         <a href='https://github.com/mafewtm' target='_blank' rel='noopener noreferrer'>
-          <Icon icon='simple-icons:github' aria-label='GitHub' />
+          <SiGithub size={16} aria-label='GitHub' />
         </a>
         <a href='https://discord.com/users/272829359117697025' target='_blank' rel='noopener noreferrer'>
-          <Icon icon='simple-icons:discord' aria-label='Discord' />
+          <SiDiscord size={16} aria-label='Discord' />
         </a>
         <a href='mailto:matthew@mafew.dev' target='_blank' rel='noopener noreferrer'>
-          <Icon icon='simple-icons:minutemailer' aria-label='Email' />
+          <SiMinutemailer size={16} aria-label='Email' />
         </a>
         <a href='https://ko-fi.com/mafewtm' target='_blank' rel='noopener noreferrer'>
-          <Icon icon='simple-icons:kofi' aria-label='Ko-fi' />
+          <SiKofi size={16} aria-label='Ko-fi' />
         </a>
       </div>
     </div>
