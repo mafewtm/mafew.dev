5aa0f8056ee072b0b30d8698b0fb5c874e94ac69
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-10-17 02:17:22 -0400

    fix: build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   base: './',
-  publicDir: false,
   build: {
     outDir: 'build',
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
